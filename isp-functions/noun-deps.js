0d8f6991d253eb4400d1cf5c708dfebc4085af68
Daniel G. Taylor <dtaylor@istreamplanet.com>
2020-02-20 10:16:23 -0800

    fix: refactor noun check to handle more edge cases

--- isp-functions/noun-deps.js
+++ isp-functions/noun-deps.js
@@ -21,43 +21,52 @@ const VERBS = [
 ];
 
 // The noun function takes the name of something and returns an error if it
-// appears to not be a plural noun.
+// appears to not be a plural noun. Singular nouns are allowed at the end of
+// a path.
 module.exports = targetValue => {
-  const value = targetValue.split('/').pop();
+  const errors = [];
 
-  if (value.startsWith('{')) {
-    // This is a path parameter. Skip it!
+  // Split into path pieces ignoring blank/empty ones and params.
+  let pieces = targetValue.split('/').filter(i => !!i);
+
+  if ((pieces.length === 1 && pieces[0] === 'search') || pieces[0] === 'me') {
+    // Top-level exceptions. Skip.
     return;
   }
 
-  // Test each verb to see if the value matches or begins with it. The
-  // regex below is to prevent e.g. `giver` from triggering the error
-  // since it is a noun that begins with a verb.
-  for (const verb of VERBS)
-    if (
-      (value.length === verb.length && value === verb) ||
-      (value.length > verb.length &&
-        value.startsWith(verb) &&
-        value[verb.length].match(/[ -_A-Z]/))
-    ) {
-      return [
-        {
+  // No piece should contain verbs.
+  for (const value of pieces) {
+    if (value.startsWith('{')) continue;
+
+    // Test each verb to see if the value matches or begins with it. The
+    // regex below is to prevent e.g. `giver` from triggering the error
+    // since it is a noun that begins with a verb.
+    for (const verb of VERBS) {
+      if (
+        (value.length === verb.length && value === verb) ||
+        (value.length > verb.length &&
+          value.startsWith(verb) &&
+          value[verb.length].match(/[ -_A-Z]/))
+      ) {
+        errors.push({
           message: `${value} should be a noun`
-        }
-      ];
-    }
-
-  // Pluralization exceptions.
-  if (value === 'me' || value === 'search') {
-    return;
-  }
-
-  const plural = inflection.pluralize(value);
-  if (value !== plural) {
-    return [
-      {
-        message: `${value} should be plural: ${plural}`
+        });
       }
-    ];
+    }
   }
+
+  // Only the last piece is allowed to be singular.
+  for (const value of pieces.slice(0, -1)) {
+    if (value.startsWith('{')) continue;
+
+    // Ensure words are plural if later paths exist.
+    const plural = inflection.pluralize(value);
+    if (value !== plural) {
+      errors.push({
+        message: `${value} should be plural: ${plural}`
+      });
+    }
+  }
+
+  return errors.length ? errors : null;
 };
